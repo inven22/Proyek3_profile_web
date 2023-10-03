36a90edddc8a5d7e899cfb2fc5ec4a28732ddf11
ahmad al fajri <116804256+inven22@users.noreply.github.com>
2023-10-03 22:48:57 +0700

    Update slide1controller.php

--- app/Http/Controllers/slide1controller.php
+++ app/Http/Controllers/slide1controller.php
@@ -5,27 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 
+
 class slide1controller extends Controller
 {
 
     public function tampil()
     {
-       return view('ahmad.inforrmasi,pendidikan');
+        return view('ahmad.inforrmasi,pendidikan');
     }
+   
 
     public function store(Request $request)
     {
         $request->validate([
             // Validasi untuk data pribadi
             'nama_lengkap' => 'required',
-            // Tambahkan validasi untuk data pendidikan
-            'universitas' => 'required',
-            'gelar' => 'required',
-            'bidang_studi' => 'required',
-            'tanggal_mulai' => 'required',
-            'tanggal_akhir' => 'required',
-            'deskripsi' => 'required',
-            // Validasi untuk data pribadi yang sudah ada
+            'nama_panggilan' => 'required',
             'tempat_lahir' => 'required',
             'tanggal_lahir' => 'required',
             'agama' => 'required',
@@ -34,9 +29,16 @@ class slide1controller extends Controller
             'provinsi' => 'required',
             'kab_kota' => 'required',
             'kode_pos' => 'required',
+           
+    
+            // Validasi untuk file (gunakan sertifikat[][], dokumen_pendukung[][] dan file_portofolio[][])
+            'sertifikat.*' => 'required|mimes:pdf,doc,docx,jpg,png',
+            'dokumen_pendukung.*' => 'required|mimes:pdf,doc,docx,jpg,png',
+            'file_portofolio.*' => 'required|mimes:pdf,doc,docx,jpg,png',
         ]);
     
-        DB::table('cv')->insert([
+        // Menyimpan data pribadi ke dalam variabel
+        $personalData = [
             'nama_lengkap' => $request->nama_lengkap,
             'nama_panggilan' => $request->nama_panggilan,
             'tempat_lahir' => $request->tempat_lahir,
@@ -47,20 +49,59 @@ class slide1controller extends Controller
             'provinsi' => $request->provinsi,
             'kab_kota' => $request->kab_kota,
             'kode_pos' => $request->kode_pos,
-        ]);
+          
+        ];
+
+        $riwayatPendidikan = [];
+
+        // Loop melalui data riwayat pendidikan yang dikirimkan melalui form
+        for ($i = 0; $i < count($request->jenjang); $i++) {
+            $riwayatPendidikan[] = [
+                'jenjang' => $request->jenjang[$i],
+                'sekolah' => $request->sekolah[$i],
+                'gelar' => $request->gelar[$i],
+                'bidang_studi' => $request->bidang_studi[$i],
+                'tanggal_mulai' => $request->tanggal_mulai[$i],
+                'tanggal_akhir' => $request->tanggal_akhir[$i],
+                'deskripsi' => $request->deskripsi[$i],
+            ];
+        }
     
-        // Simpan data pendidikan
-        DB::table('cv')->insert([
-            'universitas' => $request->universitas,
-            'gelar' => $request->gelar,
-            'bidang_studi' => $request->bidang_studi,
-            'tanggal_mulai' => $request->tanggal_mulai,
-            'tanggal_akhir' => $request->tanggal_akhir,
-            'deskripsi' => $request->deskripsi,
-        ]);
+        // Menyimpan tiga file yang diunggah dalam satu kolom JSON
+        $fileData = [
+            'sertifikat' => [],
+            'dokumen_pendukung' => [],
+            'file_portofolio' => [],
+        ];
+    
+        if ($request->hasfile('sertifikat')) {
+            foreach ($request->file('sertifikat') as $file) {
+                $fileData['sertifikat'][] = $file->store('uploads');
+            }
+        }
+    
+        if ($request->hasfile('dokumen_pendukung')) {
+            foreach ($request->file('dokumen_pendukung') as $file) {
+                $fileData['dokumen_pendukung'][] = $file->store('uploads');
+            }
+        }
+    
+        if ($request->hasfile('file_portofolio')) {
+            foreach ($request->file('file_portofolio') as $file) {
+                $fileData['file_portofolio'][] = $file->store('uploads');
+            }
+        }
+    
+        // Menggabungkan data pribadi dan data file ke dalam satu array
+        $personalData['file_data'] = json_encode($fileData);
+        $personalData['riwayat_pendidikan'] = json_encode($riwayatPendidikan);
+    
+        // Menyimpan data ke dalam tabel cv_personal menggunakan DB Facade
+        DB::table('cv')->insert($personalData);
     
         return redirect()->route('cv.tambah1')
-            ->with('success', 'Data buku berhasil ditambahkan.');
+            ->with('success', 'Data berhasil ditambahkan.');
     }
+    
     
 }
